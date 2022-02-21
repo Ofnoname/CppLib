d87141cbb7e6500b5ba8377a92e00f3daefd37f6
Ofnoname <48371590+Ofnoname@users.noreply.github.com>
2022-02-21 16:48:53 +0800

    合并一些其他计划

---
+++ OOP/vectorint.cpp
@@ -0,0 +1,31 @@
+#include <cstdlib>
+#include <cstdio>
+
+class vectorint
+{
+	public:
+	using index = unsigned;
+
+	vectorint();
+	void push_back(int x) {
+
+	}
+	int& operator[](index i) {
+
+	}
+
+	size_t size() const {
+
+	}
+	size_t capacity() const {
+
+	}
+	void shrink() {
+
+	}
+	~vectorint();
+};
+
+main() {
+
+}
