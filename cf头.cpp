c28cbd5def3a8eb9e50741133a8edd8b46305d99
Ofnoname <48371590+Ofnoname@users.noreply.github.com>
2022-03-07 20:10:36 +0800

    添加小小火车头

---
+++ cf头.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+#include <string>
+using namespace std;
+
+#define it(i, beg, end) for(int i = beg; i < end; ++i)
+#define IT(i, beg, end) for(int i = beg; i > end; --i)
+
+const int MAX = 1e5 + 3;
+
+void work() {
+
+}
+
+main() {
+	#ifndef ONLINE_JUDGE
+	freopen("in.txt", "r", stdin);
+	#endif
+	
+	int t; cin >> t; while (t--) 
+	work();
+}
