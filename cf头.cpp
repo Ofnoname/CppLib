4c43723bcaeca1931d6b21266ca3d405aa186fc3
Ofnoname <3479098865@qq.com>
2022-03-16 08:38:08 +0800

    修改cf头

--- cf头.cpp
+++ cf头.cpp
@@ -1,24 +1,26 @@
-#include <iostream>
-#include <algorithm>
-#include <numeric>
-#include <vector>
-#include <string>
-using namespace std;
+#define HEAD
+#ifdef HEAD
+	#include <bits/stdc++.h>
+	using namespace std;
 
-#define it(i, beg, end) for(int i = beg; i < end; ++i)
-#define IT(i, beg, end) for(int i = beg; i > end; --i)
+	#define it(i, beg, end) for(int i = beg; i < end; ++i)
+	#define IT(i, beg, end) for(int i = beg; i > end; --i)
+	#define int long long
+	#define uint unsigned long long
+	#define vecint vector<int>
+#endif
 
 const int MAX = 1e5 + 3;
 
 void work() {
-
+	
 }
 
 main() {
 	#ifndef ONLINE_JUDGE
 	freopen("in.txt", "r", stdin);
 	#endif
-	
+
 	int t; cin >> t; while (t--) 
 	work();
 }
