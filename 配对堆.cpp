45e30b1a748502ca2c42a48f44d42b714968dea5
Ofnoname <3479098865@qq.com>
2022-03-25 11:04:48 +0800

    修改配对堆

--- 配对堆.cpp
+++ 配对堆.cpp
@@ -1,13 +1,13 @@
 class pairheap
 {
 	struct node { //! 左儿子右兄弟表示法
-		int val;
+		int val; //* 类型
 		node *l, *r, *lson;
 	};
 	private:
 		node *root;
 		auto merge(node *p, node *q) { //! 大的合并给小的， 另外一个将不再可用
-			if (p->val > q->val) swap(p, q);
+			if (p->val > q->val) swap(p, q); //* 控制大小根
 			q->l = p, q->r = p->lson; 
 			if (p->lson) p->lson->l = q;
 			p->lson = q; return p;
@@ -23,17 +23,22 @@ class pairheap
 	public:
 		pairheap(): root(0) {}
 		
-		void push(int val) {
+		void push(auto val) {
 			auto p = new node{val};
 			root = root ? merge(p, root) : p;
 		}
 		auto pop() {
-			int val = root->val;
+			auto val = root->val;
+
 			vector<node*> t;
 			for (auto p = root->lson; p; p = p->r) t.push_back(p);
+			
 			delete root;
-			if (!t.empty()) root = placemerge(t.begin(), t.end());
-			else root = 0;
+			root = t.empty() ? 0 : placemerge(t.begin(), t.end());
+
 			return val;
+		}
+		~pairheap() {
+			//! 知道要写，但是没有写
 		}
 };
