0a323b02fb1adc8410cdff4f83341f842c306750
Ofnoname <3479098865@qq.com>
2022-04-12 11:08:11 +0800

    kmp匹配其他串

--- kmp.cpp
+++ kmp.cpp
@@ -3,6 +3,7 @@ class kmp
 private:
 	const string &s;
 	vector<int> nxt;
+
 public:
 	kmp(const string &_s): s(_s) {
 		auto lst(0);
@@ -14,6 +15,17 @@ public:
 			nxt.push_back((s[lst] == s[i]) ? ++lst : 0);
 		}
 	}
+
+	void match(const string &S) { //! 匹配长串S
+		auto lst(0), ans(0);
+	
+		for (int i = 0; i < S.size(); ++i) {
+			while (lst && (lst==s.size() || S[i]!=s[lst])) lst = nxt[lst-1];
+			if (S[i]==s[lst]) ++lst;
+			if (lst == s.size()) ++ans; //! 匹配成功
+		} cout << ans;
+	}
+
 	void print() {
 		for (auto i : nxt) cout << i << ' ';
 	}
